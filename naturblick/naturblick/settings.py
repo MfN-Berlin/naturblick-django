dbcac033a94481f69612fca4b7909c6cc168d5d1
J.Ebbighausen <j.ebbighausen@mfn.berlin>
2024-11-05 16:11:42 +0100

    flatten dir hierarchy

--- naturblick/naturblick/settings.py
+++
@@ -1,8 +0,0 @@
-import os
-
-env = os.getenv('DJANGO_ENV', 'production')
-
-if env == 'production':
-    from .prodsettings import *
-else:
-    from .devsettings import *
---
+++ naturblick/settings.py
@@ -0,0 +1,11 @@
+import os
+
+env = os.getenv('DJANGO_ENV')
+
+match env:
+    case 'production':
+        from .prodsettings import *
+    case 'development':
+        from .devsettings import *
+    case _:
+        raise Exception('Missing environment variable DJANGO_ENV')
