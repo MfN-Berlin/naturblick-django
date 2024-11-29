c4c35958c6dad1d0d65aa7ae1512ce78e2ab29a2
J.Ebbighausen <j.ebbighausen@mfn.berlin>
2024-11-29 14:37:24 +0100

    add validators png mp3

--- species/validators.py
+++ species/validators.py
@@ -1,3 +1,6 @@
+import os
+
+from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
@@ -7,3 +10,16 @@ def min_max(min_value, max_value):
         MaxValueValidator(max_value)
     ]
 
+
+def validate_png(value):
+    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
+    valid_extensions = ['.png']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError('Unsupported file extension.')
+
+
+def validate_mp3(value):
+    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
+    valid_extensions = ['.mp3']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError('Unsupported file extension.')
