cbd3bc8e503107eb6dcb4adfbdb29d045220ac96
j-ebbighausen-mfn <j.ebbighausen@mfn.berlin>
2025-04-17 10:22:28 +0200

    adding multiple species endpoint

--- species/urls.py
+++ species/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from .views import app_content_db, TagsList, PortraitDetail, SimpleTagsList, SpeciesList, \
-    AppContentCharacterValue, species
+    AppContentCharacterValue, species, species_list
 
 urlpatterns = [
-    path('species/', SpeciesList.as_view()),
+    path('species/filter/', SpeciesList.as_view()),
+    path('species/', species_list),
     path('species/<int:id>/', species),
     path('species/portrait/', PortraitDetail.as_view()),
     path('tags/filter/', TagsList.as_view()),
