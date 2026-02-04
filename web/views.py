012754540a32da4295f8682b3ce1f1d34bf9f449
j-ebbighausen-mfn <j.ebbighausen@mfn.berlin>
2026-02-04 17:40:47 +0100

    base_page template

--- web/views.py
+++ web/views.py
@@ -3,12 +3,15 @@ from typing import Any
 
 import requests
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import redirect
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.template import TemplateDoesNotExist
+from django.template.loader import get_template
 from django.urls import reverse
+from django.utils import translation
+from django.utils.translation import gettext as _
 
 from species.models import Species, Portrait
-from django.utils.translation import gettext as _
 
 
 class Og:
@@ -99,6 +102,44 @@ def map_page(request, obs_id):
     return default_response(request, ogs_list)
 
 
+def about(request):
+    return web_render(request, "about")
+
+
+def mobileapp(request):
+    return web_render(request, "mobileapp")
+
+
+def faq(request):
+    return web_render(request, "faq")
+
+
+def kontakt(request):
+    return web_render(request, "contact")
+
+
+def privacy(request):
+    return web_render(request, "privacy")
+
+
+def imprint(request):
+    return web_render(request, "imprint")
+
+
+def web_render(request, template: str) -> HttpResponse:
+    lang = translation.get_language()
+    try:
+        get_template(f"web/{template}.{lang}.html")
+        return render(request, f"web/{template}.{lang}.html")
+    except TemplateDoesNotExist:
+        print("Fallback to DE ")
+        return render(request, f"web/{template}.de.html")
+
+
+def digitalaccessibilitystatement(request):
+    return web_render(request, "digitalaccessibilitystatement")
+
+
 def default_response(request, more_ogs=None):
     if more_ogs is None:
         more_ogs = []
@@ -112,6 +153,7 @@ def default_ogs(request: WSGIRequest) -> list[Og]:
             Og("og:site-name", "Naturblick"),
             Og("og:url", og_url(request))
             ]
+
 
 def extract_language(request: WSGIRequest | Any):
     allowed_langs = {"de", "en", "dels"}
@@ -127,6 +169,7 @@ def og_url(request):
 
 def seen_by(user, date, coords):
     return _("Seen by {user} at {date} in {coords}").format(user=user, date=date, coords=coords)
+
 
 def add_image_ogs(request, ogs_list, image):
     ogs_list.append(Og("og:image:width", image.width))
