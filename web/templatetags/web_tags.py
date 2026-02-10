dd0b3a9ea48bd7c9e4b2d9f585a3eded3a9b3415
j-ebbighausen-mfn <j.ebbighausen@mfn.berlin>
2026-02-10 13:09:05 +0100

    header styling part 1

--- web/templatetags/web_tags.py
+++ web/templatetags/web_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.contrib.staticfiles import finders
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -21,3 +23,11 @@ def toggle_dels(request):
 
     query = params.urlencode()
     return f"{request.path}?{query}" if query else request.path
+
+@register.simple_tag
+def inline_svg(path):
+    full_path = finders.find(path)
+    if full_path:
+        with open(full_path, 'r', encoding='utf-8') as f:
+            return mark_safe(f.read())
+    return ''
