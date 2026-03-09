638febe9987faf39a98d621894c734e5c350b213
j-ebbighausen-mfn <j.ebbighausen@mfn.berlin>
2026-03-09 09:22:30 +0100

    safeguard lang middleware

--- web/langmiddleware.py
+++ web/langmiddleware.py
@@ -1,9 +1,13 @@
+from django.core.exceptions import BadRequest
 from django.utils import translation
 
 def set_lang_middleware(get_response):
+    availble_langs = [ "de", "en", "dels"]
 
     def middleware(request):
         lang = request.GET.get('lang') if request.GET.get('lang') else "de"
+        if lang not in availble_langs:
+            return BadRequest("Language not available")
         translation.activate(lang)
         response = get_response(request)
 
