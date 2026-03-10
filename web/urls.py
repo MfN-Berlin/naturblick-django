03a9630c9977502ac5b70ada9a10053411b9725f
j-ebbighausen-mfn <j.ebbighausen@mfn.berlin>
2026-03-10 16:41:36 +0100

    named map_proxy

--- web/urls.py
+++ web/urls.py
@@ -4,7 +4,7 @@ from web import views
 
 urlpatterns = [
     path("dev", views.index, name="index"),
-    path("dev/map_proxy", views.map_proxy),
+    path("dev/map_proxy", views.map_proxy, name="map_proxy"),
     re_path(
         r'^dev/species/portrait/(?P<id>[0-9]+)$', views.portrait, name="portrait"),
     path("dev/faq", views.faq, name="faq"),
