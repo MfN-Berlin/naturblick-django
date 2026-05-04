af8c792839a48c39a482401f04f93a610c0c8175
j-ebbighausen-mfn <j.ebbighausen@mfn.berlin>
2026-05-04 14:48:15 +0200

    working

--- web/static/web/js/web.js
+++ web/static/web/js/web.js
@@ -1,4 +1,4 @@
-function font_zoom_toggle() {
+function fontZoomToggle() {
     const root = document.documentElement;
     const styles = getComputedStyle(root);
     const def = styles.getPropertyValue('--default-font-zoom').trim();
@@ -7,14 +7,33 @@ function font_zoom_toggle() {
     let next;
     if ((saved ?? def) === def) {
         next = zoomed
-        root.style.setProperty('--zoomed-color', "#ccc");
-        root.style.setProperty('--unzoomed-color', "inherit");
+        document.documentElement.style.setProperty('--zoomed-color', "#ccc");
+        document.documentElement.style.setProperty('--unzoomed-color', "inherit");
     } else {
         next = def
         root.style.setProperty('--font-zoom', next);
-        root.style.setProperty('--zoomed-color', "inherit");
-        root.style.setProperty('--unzoomed-color', "#ccc");
+        document.documentElement.style.setProperty('--zoomed-color', "inherit");
+        document.documentElement.style.setProperty('--unzoomed-color', "#ccc");
     }
     root.style.setProperty('--font-zoom', next);
     localStorage.setItem('font-zoom', next);
 }
+
+function initFontZoom() {
+    const root = document.documentElement;
+    const styles = getComputedStyle(root);
+    const def = styles.getPropertyValue('--default-font-zoom').trim();
+    const zoomed = styles.getPropertyValue('--zoomed-font-zoom').trim();
+    const saved = localStorage.getItem('font-zoom');
+    if ((saved ?? def) === def) {
+        root.style.setProperty('--font-zoom', def);
+        document.documentElement.style.setProperty('--zoomed-color', "inherit");
+        document.documentElement.style.setProperty('--unzoomed-color', "#ccc");
+    } else {
+        root.style.setProperty('--font-zoom', zoomed);
+        document.documentElement.style.setProperty('--zoomed-color', "#ccc");
+        document.documentElement.style.setProperty('--unzoomed-color', "inherit");
+    }
+}
+
+initFontZoom();
