25fd4206735cbb247413b31bab25ae7b00b9340a
j-ebbighausen-mfn <j.ebbighausen@mfn.berlin>
2026-06-23 10:06:24 +0200

    simplify endpoint for volunteer tool

--- species/serializers.py
+++ species/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
+
 from .models import Group
+
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ["name", "image", "svg"]
+
+
+class DescMetaSerializer(serializers.Serializer):
+    image_orig = serializers.URLField(source="descmeta.image_file.image.url", read_only=True)
+    image_large = serializers.URLField(source="descmeta.image_file.image_large.url", read_only=True)
--- species/urls.py
+++ species/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from .views import app_content_db, app_content_leicht_db, app_content_leicht_image_list, \
-    AppContentCharacterValue, GroupsList
+    AppContentCharacterValue, GroupsList, VolunteerImages
 
 urlpatterns = [
     path("app-content/db/", app_content_db, name="app-content-db"),
     path("app-content/leicht-db/", app_content_leicht_db, name="app-content-leicht-db"),
     path("app-content/leicht-image-list/", app_content_leicht_image_list, name="app-content-leicht-image-list"),
     path("app-content/character-values/", AppContentCharacterValue.as_view()),
-    path("app-content/groups/", GroupsList.as_view(), name="groups")
+    path("app-content/groups/", GroupsList.as_view(), name="groups"),
+    path('species/volunteerimages/', VolunteerImages.as_view(), name="volunteer-images"),
 ]
